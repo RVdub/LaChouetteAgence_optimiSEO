f33df84534c068dc2eab3f6811d439b3ee2cf6be
RVdub <dhcontrole@gmail.com>
2020-07-11 23:00:07 +0200

    code reviewed from an SEO perspective

--- includes/form_1.php
+++ includes/form_1.php
@@ -4,19 +4,19 @@
 		return false;
 	}
 	
-	$name = $_POST['name'];
-	$email = $_POST['email'];
-	$input_504 = $_POST['input_504'];
-	$message = $_POST['message'];
+	$name = htmlspecialchars($_POST['name']);
+	$email = htmlspecialchars($_POST['email']);
+	$input_504 = htmlspecialchars($_POST['input_504']);
+	$message = htmlspecialchars($_POST['message']);
 	
-	$to = 'receiver@yoursite.com'; // Email submissions are sent to this email
+	$to = 'dhcontrole@gmail.com'; // Email submissions are sent to this email
 
 	// Create email	
 	$email_subject = "Message from your website";
 	$email_body = "You have received a new message. \n\n".
 				  "Name: $name \nEmail: $email \nInput_504: $input_504 \nMessage: $message \n";
 	$headers = "MIME-Version: 1.0\r\nContent-type: text/plain; charset=UTF-8\r\n";	
-	$headers .= "From: contact@yoursite.com\n";
+	$headers .= "From: contact@lachouetteagence.com\n";
 	$headers .= "Reply-To: $input_504";	
 	
 	mail($to,$email_subject,$email_body,$headers); // Post message
